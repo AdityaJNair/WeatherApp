7707efe9f63cc6df9139d2164ef913564767e73a
AdityaJNair <adijnair@gmail.com>
2016-08-31 17:25:48 +1200

    working rest api calls to weather api

--- js/basic.js
+++ js/basic.js
@@ -1,45 +1,71 @@
-var button = $("#btnstopid");
-var textparagraph = $("#para");
+var subscriptionKey = "a5c73d2705c60263ede71ef16dee8137";
 
-$("#btnstopid").on('click', function () {
-    if (isNaN($("#stopid").val())){
-        $("#stopid").val("");
-        $("#stopid").attr("placeholder", "Enter a number e.g. 7150");
-        console.log("failed by not a number");
-    } else if ($("#stopid").val() < 1){
-        $("#stopid").val("");
-        $("#stopid").attr("placeholder", "Enter text here");
-        console.log("failed by being empty");
-    } else {
-        console.log("success here");
-        console.log($("#stopid").val());
-        processStopID($("#stopid").val());
+$(document).ready(function () {
+    if (navigator.geolocation) {
+        console.log("in navigation");
+        navigator.geolocation.getCurrentPosition(findPosition, fail);
     }
 });
 
-function processStopID(number){
-        var params = {
-            // Request parameters
-            "callback": "{string}",
-        };
-      
-        $.ajax({
-            url: "https://api.at.govt.nz/v2/gtfs/stops/stopId/" + number,
-            beforeSend: function(xhrObj){
-                // Request headers
-                xhrObj.setRequestHeader("Content-Type","application/json");
-                xhrObj.setRequestHeader("Accept","application/json");
-                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","3c10f424470a439aa15ee65c1be93de3");
-            },
-            contentType: 'application/json; charset=utf-8',
-            type: "GET",
-        })
-        .done(function(data) {
-            alert("success");
-            console.log("done data");
-        })
-        .fail(function() {
+function setInfo(weatherInfo) {
+    $("#temperature").text(weatherInfo.temperature);
+    $("#location").text(weatherInfo.place);
+    $("#humidity").text(weatherInfo.humidity);
+    $("#wind").text(weatherInfo.windSpeed);
+    $("#directionofwind").text(weatherInfo.windDirection);
+    $("#hightemp").text(weatherInfo.temp_max);
+    $("#lowtemp").text(weatherInfo.temp_min);
+    $("#country").text(weatherInfo.country);
+    $("#pressure").text(weatherInfo.pressure);
+}
+
+function updateUsingCurrentPosition(latitude, longitude) {
+    $.ajax({
+        dataType: "jsonp",
+        type: "GET",
+        url: "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + subscriptionKey,
+        success: function (data) {
+            console.log("in success");
+            console.log(data.main.humidity);
+            console.log(data.main.temp);
+            console.log(data.wind.speed);
+            console.log(data.name);
+            console.log(data.wind.deg);
+            console.log(data.sys.country);
+            console.log(data.main.temp_min);
+            console.log(data.main.temp_max);
+            console.log(data.main.pressure);
+
+            var weatherInfo = {};
+            weatherInfo.humidity = data.main.humidity;
+            weatherInfo.temperature = Math.round(data.main.temp) - 273 + "°C";
+            weatherInfo.windSpeed = data.wind.speed + " m/s";
+            weatherInfo.place = data.name;
+            weatherInfo.windDirection = data.wind.deg;
+            weatherInfo.country = data.sys.country;
+            weatherInfo.temp_min = Math.round(data.main.temp_min) - 273 + "°C";
+            weatherInfo.temp_max = Math.round(data.main.temp_max) - 273 + "°C";
+            weatherInfo.pressure = data.main.pressure + " hpa";
+            setInfo(weatherInfo);
+        },
+        error: function () {
             alert("error");
-            console.log("failed data");
-        });
+        }
+    });
+
+}
+
+function findPosition(pos) {
+    var coordinates = pos.coords;
+    var crd = pos.coords;
+
+    console.log('Your current position is:');
+    console.log('Latitude : ' + crd.latitude);
+    console.log('Longitude: ' + crd.longitude);
+    console.log('More or less ' + crd.accuracy + ' meters.');
+    updateUsingCurrentPosition(crd.latitude, crd.longitude);
+}
+
+function fail() {
+    alert("failed to get geo");
 }
