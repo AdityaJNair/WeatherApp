1f057a9d4adf208ccb6e10689e85e5447d8b2694
AdityaJNair <adijnair@gmail.com>
2016-09-01 14:09:22 +1200

    changed to https

--- js/basic.js
+++ js/basic.js
@@ -3,7 +3,6 @@ var subscriptionKey = "a5c73d2705c60263ede71ef16dee8137";
 $(document).ready(function () {
                
     if (navigator.geolocation) {
-        console.log("in navigation");
         navigator.geolocation.getCurrentPosition(findPosition, fail);
     } else {
            $("#head").text("Please enable geolocation for this application to work");
@@ -62,20 +61,8 @@ function updateUsingCurrentPosition(latitude, longitude) {
     $.ajax({
         dataType: "jsonp",
         type: "GET",
-        url: "http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + subscriptionKey,
+        url: "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + subscriptionKey,
         success: function (data) {
-            console.log("in success");
-            console.log(data.main.humidity);
-            console.log(data.main.temp);
-            console.log(data.wind.speed);
-            console.log(data.name);
-            console.log(data.wind.deg);
-            console.log(data.sys.country);
-            console.log(data.main.temp_min);
-            console.log(data.main.temp_max);
-            console.log(data.main.pressure);
-            console.log(data.weather[0].icon);
-
             var weatherInfo = {};
             weatherInfo.humidity = data.main.humidity;
             weatherInfo.temperature = Math.round(data.main.temp) - 273 + "°C";
@@ -101,13 +88,8 @@ function updateUsingCurrentPosition(latitude, longitude) {
     $.ajax({
         dataType: "jsonp",
         type: "GET",
-        url: "http://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + subscriptionKey,
+        url: "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + subscriptionKey,
         success: function (data) {
-            console.log(formatDate(data.list[8].dt_txt));
-            console.log(formatDate(data.list[16].dt_txt));
-            console.log(formatDate(data.list[24].dt_txt));
-            console.log(formatDate(data.list[32].dt_txt));
-
             var weatherInfoD2 = {};
             var weatherInfoD3 = {};
             var weatherInfoD4 = {};
@@ -158,18 +140,13 @@ function updateUsingCurrentPosition(latitude, longitude) {
 function findPosition(pos) {
     var coordinates = pos.coords;
     var crd = pos.coords;
-
-    console.log('Your current position is:');
-    console.log('Latitude : ' + crd.latitude);
-    console.log('Longitude: ' + crd.longitude);
-    console.log('More or less ' + crd.accuracy + ' meters.');
     $("#latitude").text(crd.latitude);
     $("#longitude").text(crd.longitude); 
     updateUsingCurrentPosition(crd.latitude, crd.longitude);
 }
 
 function fail() {
-    alertify.alert("Unable to get your current position, Maybe try again in a few minutes or in another location?");
+    alertify.alert("Unable to get your current position. Try a different browser (not chrome) or maybe try again in a few minutes or in another location?");
 }
 
 function toTitleCase(str) {
